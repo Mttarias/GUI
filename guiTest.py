fe89e24fc7f50b847524f85fb46a3b8bb292425a
Mttarias <mattheus.berr001@mymdc.net>
2023-04-30 00:38:59 -0400

    cost of auto is now variable and updates on label

--- guiTest.py
+++ guiTest.py
@@ -1,25 +1,34 @@
 import tkinter as tk
+import math
 
+#Increments number by 1
 def increase():
     value = int(lbl_value["text"])
     lbl_value["text"] = f"{value + 1}"
-    if int(lbl_value["text"]) >= 10:
+    if int(lbl_value["text"]) >= cost:
         btn_auto['state'] = tk.NORMAL
-    
+
+#Recursive function that acts as an automatic incrementer
 def autoIncrement():
     value = int(lbl_value["text"])
     lbl_value["text"] = f"{value + 1}"
     window.after(1000, autoIncrement)
-    if int(lbl_value["text"]) >= 10:
+    if int(lbl_value["text"]) >= cost:
         btn_auto['state'] = tk.NORMAL
     
+#Defines logic for auto increment button
 def pay():
+    global cost
     value = int(lbl_value["text"])
-    lbl_value["text"] = f"{value - 10}"
+    lbl_value["text"] = f"{value - cost}"
     window.after(1000, autoIncrement)
     btn_auto["state"] = tk.DISABLED
+    cost = math.ceil(cost * 1.25)
+    #Updates the display label for the increased cost
+    lbl2.config(text=f"Cost: {cost}")
 
 window = tk.Tk()
+cost = 10
 
 window.rowconfigure(0, minsize=50, weight=1)
 window.columnconfigure([0, 1, 2], minsize=50, weight=1)
@@ -27,7 +36,7 @@ window.columnconfigure([0, 1, 2], minsize=50, weight=1)
 lbl_value = tk.Label(master=window, text="0")
 lbl_value.grid(row=0, column=1)
 
-lbl2 = tk.Label(master=window, text="Cost 10")
+lbl2 = tk.Label(master=window, text=f"Cost: {cost}")
 lbl2.grid(row=2, column=0)
 
 btn_auto = tk.Button(master=window,
